65abc478dbe0c9334536ca13869da02b651bd58a
Conwyn <robertgordins@gmail.com>
2020-06-08 19:12:35 +0300

    Added first migrations to the project. Changed project name and other settings.

---
+++ database/migrations/2020_06_08_154735_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('page_id')->constrained();
+            $table->string('title', 100);
+            $table->text('short_description')->nullable();
+            $table->mediumText('description')->nullable();
+            $table->integer('rating')->default(0);
+            $table->boolean('isRecipe')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/migrations/2020_06_08_154751_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+            $table->foreignId('post_id')->constrained();
+            $table->string('title',100);
+            $table->text('description')->nullable();
+            //Image file data
+            $table->string('filename');
+            $table->string('mime');
+            $table->string('original_filename');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
